24e934443db4773b7ec06b7bf1d30ff2ae22685b
Sergey Grebenshchikov <sgreben@gmail.com>
2018-06-17 16:01:57 +0200

    Skip invalid values in flatten()

--- cmd/jp/split.go
+++ cmd/jp/split.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"reflect"
-)
+import "reflect"
 
 var indexableKind = map[reflect.Kind]bool{
 	reflect.Slice: true,
@@ -12,15 +10,18 @@ var indexableKind = map[reflect.Kind]bool{
 func flatten(in [][]reflect.Value) (out []reflect.Value) {
 	for _, a := range in {
 		for _, v := range a {
+			if !v.IsValid() {
+				continue
+			}
 			if indexableKind[v.Kind()] {
 				sub := make([]reflect.Value, v.Len())
 				for j := 0; j < v.Len(); j++ {
-					sub = append(sub, v.Index((j)))
+					sub = append(sub, v.Index(j))
 				}
 				out = append(out, flatten([][]reflect.Value{sub})...)
 				continue
 			}
-			if v.IsValid() && v.CanInterface() {
+			if v.CanInterface() {
 				if sub, ok := v.Interface().([]interface{}); ok {
 					for i := range sub {
 						out = append(out, reflect.ValueOf(sub[i]))
